ecaf36acbaa0ff8f8f1c27be9b0ee22c89178993
Cesar Murillo <namiayumis@gmail.com>
2023-03-09 16:52:02 +0900

    [add] social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,9 @@
+import { FaTwitter } from 'react-icons/fa'
+import { FaFacebook } from 'react-icons/fa'
+import { FaYoutube } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+
+
 export default function Footer()  {
   return (
     <footer className="container mx-auto bg-white py-8 border-t border-gray-400">
@@ -16,15 +22,44 @@ export default function Footer()  {
           <div className="flex w-full lg:w-1/2 lg:justify-end lg:text-right">
             <div className="px-3 md:px-0">
               <h3 className="font-bold text-gray-900">Social</h3>
-              <ul className="list-reset items-center pt-3">
+              <ul className="list-reset items-center pt-3 flex">
                 <li>
                   <a
-                    className="inline-block no-underline hover:text-black hover:underline py-1"
-                    href="#"
+                    className="p-4 inline-block no-underline hover:text-black hover:underline py-1"
+                    target="_blank" href="https://www.facebook.com/embajadadelperuenjapon"
                   >
-                    Add social links
+                    <FaFacebook className="text-blue-700 hover:text-blue-500 w-8 h-8"/>
+
                   </a>
                 </li>
+                <li>
+                  <a
+                    className="p-4 inline-block no-underline hover:text-black hover:underline py-1"
+                    target="_blank" href="https://twitter.com/peruinjapan"
+                  >
+                    <FaTwitter className='text-blue-500 w-8 h-8'/>
+
+                  </a>
+                </li>
+              
+                <li>
+                  <a
+                    className="p-4 inline-block no-underline hover:text-black hover:underline py-1"
+                    target="_blank" href="https://www.instagram.com/peruinjapan/"
+                  >
+                    <FaInstagram className="text-pink-600 hover:text-pink-400 w-8 h-8" />
+
+                  </a>
+                </li>   
+                <li>
+                  <a
+                    className="p-4 inline-block no-underline hover:text-black hover:underline py-1"
+                    target="_blank" href="https://www.youtube.com/@embajadadelperuenjapon"
+                  >
+                    <FaYoutube className="text-red-600 hover:text-red-400 w-8 h-8" />
+
+                  </a>
+                </li>               
               </ul>
             </div>
           </div>
---
+++ src/pages/cacao.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+import Cacao from "@/components/Cacao";	
+
+export default function cacaoPage () {
+  return (
+    <Layout>
+      <Cacao/>
+    </Layout>
+  )
+}
