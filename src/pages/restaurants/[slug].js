5e7344a01709e69adb41b2a44f0b07cdc29ca5b8
Cesar Murillo <namiayumis@gmail.com>
2023-03-17 08:54:01 +0900

    [create] slug restaurants

---
+++ src/pages/restaurants/[slug].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+
+const Slug = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  return <p>Slug: {slug}</p>;
+};
+
+export default Slug;
