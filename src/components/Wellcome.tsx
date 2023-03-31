c08429e25bcc219a4cd23b5583fdbeecf6eb9b12
Cesar Murillo <namiayumis@gmail.com>
2023-03-31 14:21:34 +0900

    [add] react-portable-text to WellcomeComponent

--- src/components/Wellcome.tsx
+++ src/components/Wellcome.tsx
@@ -1,13 +1,22 @@
 import { useRouter } from "next/router";
+import PortableText from "react-portable-text"
 
-export default function Wellcome() {
 
+export default function Wellcome(posts: any) {
+  console.log("post", posts)
   const { locale } = useRouter();
   return (
    <div className="container py-8 px-6 mx-auto">
     <div className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8">
-       { locale==="en" ? "WELCOME TO PERU"  : "ペルー料理ガイドへようこそ。"} 
-    </div>
+      { locale=== posts.post[1].__i18n_lang  ? "WELCOME TO PERU"  : "ペルー料理ガイドへようこそ。"} 
+      </div>
+    <PortableText 
+    content={ locale=== posts.post[1].__i18n_lang ? posts.post[1].content :posts.post[0].content}       
+    serializers={{
+      normal: (props) => <p className="mt-8 mb-8">{props.children}</p>,
+      }}/>
+      
   </div>
+
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,48 @@
-import Link from "next/link";
+import { apiVersion, dataset, projectId } from "lib/sanity.api";
+import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
+import { createClient, groq } from "next-sanity";
 import React from "react";
 
 import Carrusel from "../components/Carrusel";
 import Layout from "../components/Layout";
 import Wellcome from "../components/Wellcome";
 
-export default function Home() {
-  const { locale } = useRouter();
+const clientConfig = {
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+};
 
+function getPost() {
+  return createClient(clientConfig).fetch(groq`
+    *[_type == "post"]{
+      __i18n_lang,
+      _id,
+      title,
+      content,
+    }
+  `);
+}
+
+
+export default function Home( { posts: posts }: { posts: any }) {
+  const { locale } = useRouter();
   return (
     <Layout language={locale}>
       <Carrusel />
-      <Wellcome />
+      <Wellcome post={posts} />
     </Layout>
   );
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const posts = await getPost();
+  console.log("posts static", posts)
+  return {
+    props: {
+      posts,
+    },
+  }
 }
