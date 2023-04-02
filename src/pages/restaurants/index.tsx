15f1c1a19472cbdaf8e6ecc22ec1dbcb5bfd4a43
Cesar Murillo <namiayumis@gmail.com>
2023-04-02 22:24:04 +0900

    [add] image restaurantsPage

--- src/pages/restaurants/index.tsx
+++ src/pages/restaurants/index.tsx
@@ -1,6 +1,7 @@
 import { dataset, projectId } from "lib/sanity.api";
 import { GetStaticProps } from "next";
 import { Inter } from "next/font/google";
+import  Image from "next/image";
 import {useRouter} from "next/router";
 import { createClient, groq } from "next-sanity";
 import React from "react";
@@ -28,25 +29,29 @@ function getCities() {
 
 export default function Restaurants({ cities: cities }: { cities: any }) {
   const { locale } = useRouter();
+  //http://embperujapan.org/gastronomia/restaurantsv2.jpg
   return (
     <Layout language={locale}>
-
-      <section
-        className="w-full mx-auto bg-nordic-gray-light flex pt-12 md:pt-0 md:items-center bg-cover bg-right"
-        style={{
-          maxWidth: 1600,
-          height: "32rem",
-          backgroundImage:
-            // 'url("https://peru.info/img/20.restaurantes-del-mundo/logo-banner.jpg")',
-            'url("http://embperujapan.org/gastronomia/restaurantsv2.jpg")',
-        }}
-      >
-        <div className="container mx-auto">
-          <div className="flex flex-col w-full lg:w-1/2 justify-center items-start px-6 tracking-wide">
-            <h1 className="text-white text-4xl lg:text-5xl my-4">
-            PERUVIAN RESTAURANTS
-            </h1>
-          </div>
+      <section className="flex justify-center align-middle ">
+        <div className="">
+          <Image
+            alt="Gastronomy"
+            src="http://embperujapan.org/gastronomia/restaurantsv2.jpg"
+            width={1500}
+            height={660}
+            className="hidden md:block md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl rounded-3xl shadow-2xl"
+          />
+        </div>
+      </section>
+      <section className="flex justify-center align-middle ">
+        <div className="mx-auto block md:hidden">
+          <Image
+            alt="Gastronomy"
+            src="http://embperujapan.org/gastronomia/gastronomy500v2.jpg"
+            width={400}
+            height={380}
+            className="max-w-screen-xs rounded-3xl shadow-2xl"
+          />
         </div>
       </section>
       <section className="bg-white py-8">
