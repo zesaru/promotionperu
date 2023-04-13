d3fdca8df9cbc8206c0cbaf1d2d2879435197fdc
Cesar Murillo <namiayumis@gmail.com>
2023-04-13 16:17:10 +0900

    [add] Seo Wellcome Page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
 
@@ -18,31 +17,7 @@ export default function Layout({ children, language }: LayoutProps) {
 
   return (
     <div className="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
-      <Head>
-        <title>PeruinJapan </title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-
-	      <meta name="msapplication-TileColor" content="#da532c" />
-	      <meta name="theme-color" content="#ffffff" />
-        <meta property="og:url" content="promotionperu.vercel.app" />
-        <meta property="og:type" content="website" />
-        <meta property="fb:app_id" content="521355484545494" />
-        <meta
-          property="og:title"
-          content="WELCOME TO THE PERUVIAN GASTRONOMY GUIDE OF JAPAN."
-        />
-        <meta name="twitter:card" content="summary" />
-        <meta
-          property="og:description"
-          content="WELCOME TO THE PERUVIAN GASTRONOMY GUIDE OF JAPAN."
-        />
-        <meta property="og:image" content={"url of image"} />
-      </Head>
-      <Header  />
+      <Header />
         {children}
       <Footer />
     </div>
