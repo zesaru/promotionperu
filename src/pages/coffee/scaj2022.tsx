a0f1e3ae7567ef54fc0da5c1ef065af19cf40459
Cesar Murillo <namiayumis@gmail.com>
2023-05-10 11:46:39 +0900

    [add] alt to image Scaj2022Page

--- src/pages/coffee/scaj2022.tsx
+++ src/pages/coffee/scaj2022.tsx
@@ -61,46 +61,55 @@ const Photos = [
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_opening_roberto_seminario.jpg",
     width: 4,
-    height: 3
+    height: 3,
+    alt: "SCAJ2022 Opening Roberto Seminario"
   },
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_2022_1.jpg",
     width: 1,
-    height: 1
+    height: 1,
+    alt: "SCAJ2022"
   },
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_2022_flyer.jpg",
     width: 3,
-    height: 4
+    height: 4,
+    alt: "SCAJ2022 Flyer"
   },
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_2022_5.jpg",
     width: 3,
-    height: 4
+    height: 4,
+    alt: "SCAJ2022"
   },
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_2022_6.jpg",
     width: 3,
-    height: 4
+    height: 4,
+    alt: "SCAJ2022"
   },
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_2022.jpg",
     width: 4,
-    height: 3
+    height: 3,
+    alt: "SCAJ2022"
   },
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_2022_2.jpg",
     width: 3,
-    height: 4
+    height: 4,
+    alt: "SCAJ2022"
   },
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_2022_4.jpg",
     width: 4,
-    height: 3
+    height: 3,
+    alt: "SCAJ2022"
   },
   {
     src: "http://embperujapan.org/gastronomia/SCAJ2022/scaj_2022_3.jpg",
     width: 4,
-    height: 3
+    height: 3,
+    alt: "SCAJ2022"
   }
 ];
