6a3b157b5f3e6dcd2c20f55a9db832891f29bcd3
Cesar Murillo <namiayumis@gmail.com>
2023-05-10 15:50:06 +0900

    [add] recipes schemas

---
+++ schemas/documents/recipes.ts
@@ -0,0 +1,35 @@
+import { defineType } from "sanity";
+
+export default defineType({
+  type: "document",
+  name: "recipes",
+  title: "Recipes",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    },
+    {
+      name: "ingredients",
+      title: "Ingredients",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "preparation",
+      title: "Preparation",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+});
--- schemas/schemaTypes.ts
+++ schemas/schemaTypes.ts
@@ -1,6 +1,7 @@
 import cityType from "./documents/city";
 import menuType from "./documents/menu";
 import postType from "./documents/post";
+import recipesType from "./documents/recipes";
 import restaurantType from "./documents/restaurant";
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArray": true}] */
@@ -8,5 +9,6 @@ export default [
     cityType,
     menuType,
     postType,
-    restaurantType
+    restaurantType,
+    recipesType
   ];
